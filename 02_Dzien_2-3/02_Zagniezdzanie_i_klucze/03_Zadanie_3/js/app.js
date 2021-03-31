8e0e5fd85378cca6e4bdd8a2940380e7369c61d2
PiotrSciepko <piotrek.priv@gmail.com>
2021-03-31 14:37:09 +0200

    02_Dzien_2-3/02_Zagniezdzanie_i_klucze/03_Zadanie_3

--- 02_Dzien_2-3/02_Zagniezdzanie_i_klucze/03_Zadanie_3/js/app.js
+++ 02_Dzien_2-3/02_Zagniezdzanie_i_klucze/03_Zadanie_3/js/app.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import people from "./data/people";
 
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <>
+        {people.map(el =>
+            <div className={"person"} key={el.id}>
+                <img src={el.avatar}/>
+                <div className={"info"}>
+                    <h1>{el.title} {el.name} {el.surname}</h1>
+                    <p>{el.bio}</p>
+                </div>
+            </div>)}
+    </>
+    ,
+    document.getElementById("app")
 );
