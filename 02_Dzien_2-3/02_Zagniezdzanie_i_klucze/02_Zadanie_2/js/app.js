e53655bada5c2c3ed6ac532bce637d2220bc15ee
PiotrSciepko <piotrek.priv@gmail.com>
2021-03-31 14:20:35 +0200

    02_Dzien_2-3/02_Zagniezdzanie_i_klucze/02_Zadanie_2

--- 02_Dzien_2-3/02_Zagniezdzanie_i_klucze/02_Zadanie_2/js/app.js
+++ 02_Dzien_2-3/02_Zagniezdzanie_i_klucze/02_Zadanie_2/js/app.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import person from "./data/person";
+
 
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <>
+        <h1>{person.title} {person.firstName} {person.lastName}</h1>
+        <span>{person.age}</span>
+    </>
+    ,
+    document.getElementById("app")
 );
