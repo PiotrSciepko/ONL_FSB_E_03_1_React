34287fd5d097f67f8d72aef006e833ab9a1410ab
PiotrSciepko <piotrek.priv@gmail.com>
2021-03-31 12:37:29 +0200

    02_Dzien_2-3/01_Atrybuty_elementow/02_Zadanie_2

--- 02_Dzien_2-3/01_Atrybuty_elementow/02_Zadanie_2/js/app.js
+++ 02_Dzien_2-3/01_Atrybuty_elementow/02_Zadanie_2/js/app.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function genNumber() {
-  return Math.round(Math.random() * 9 + 1);
+    return Math.round(Math.random() * 9 + 1);
+}
+
+function checkResult() {
+    const a = genNumber();
+    const b = genNumber();
+    const result = parseFloat(prompt(a + " + " + b + " ="));
+    if (result === a + b) {
+        return <div style={{backgroundColor:"green"}}>Odpowiedź poprawna</div>
+    } else {
+        return <div style={{backgroundColor:"red"}}>Odpowiedź błędna</div>
+    }
 }
 
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    checkResult(),
+    document.getElementById("app")
 );
