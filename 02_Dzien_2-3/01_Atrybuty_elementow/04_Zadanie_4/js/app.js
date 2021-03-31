f566d036650af0b272abe2e60ef7def5c53d8607
PiotrSciepko <piotrek.priv@gmail.com>
2021-03-31 13:27:37 +0200

    02_Dzien_2-3/01_Atrybuty_elementow/04_Zadanie_4

--- 02_Dzien_2-3/01_Atrybuty_elementow/04_Zadanie_4/js/app.js
+++ 02_Dzien_2-3/01_Atrybuty_elementow/04_Zadanie_4/js/app.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const colors = ["red", "green", "blue"];
+const color = prompt("Podaj kolor ramki");
+let result = <div>Nieprawidłowy kolor</div>;
+if (colors.includes(color)) {
+    result =
+        <div style={{width: "100px", height: "100px", borderWidth: "5px", borderStyle: "solid", borderColor: color}}/>
+}
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    result,
+    document.getElementById("app")
 );
