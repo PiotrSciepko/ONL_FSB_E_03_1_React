9b0c6ba00672a0d1bb905037ffd767baccfdcfb2
PiotrSciepko <piotrek.priv@gmail.com>
2021-03-31 11:39:31 +0200

    01_Dzien_1/03_Wyrazenia/05_Zadanie_5

--- 01_Dzien_1/03_Wyrazenia/05_Zadanie_5/js/app.js
+++ 01_Dzien_1/03_Wyrazenia/05_Zadanie_5/js/app.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const a = parseFloat(prompt("Podaj pierwszą liczbę"));
+const sign = prompt("Podaj pierwszą liczbę");
+const b = parseFloat(prompt("Podaj pierwszą liczbę"));
+
+const calc = (a, b, sign) => {
+    switch (sign) {
+        case "+":
+            return <h1>{a + b}</h1>;
+        case "-":
+            return <h2>{a - b}</h2>;
+        case "*":
+            return <h3>{a * b}</h3>;
+        case "/":
+            return <h4>{a / b}</h4>;
+    }
+}
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    calc(a, b, sign),
+    document.getElementById("app")
 );
