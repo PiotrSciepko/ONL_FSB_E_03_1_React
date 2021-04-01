9050d36f975428afa9f12458738713de3543be93
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-01 17:48:48 +0200

    03_Dzien_4/02_Props/04_Zadanie_4

--- 03_Dzien_4/02_Props/04_Zadanie_4/js/app.js
+++ 03_Dzien_4/02_Props/04_Zadanie_4/js/app.js
@@ -1,7 +1,16 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+const LikeBox = (props) => {
+    return (
+        <div style={{textAlign: "center"}}>
+            <span>{props.initialNumber} likes</span><br/>
+            <button>Lubię to!</button>
+        </div>
+    )
+}
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <LikeBox initialNumber={101}/>,
+    document.getElementById("app")
 );
