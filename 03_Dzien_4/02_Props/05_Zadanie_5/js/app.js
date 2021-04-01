2be33e51b30a0f3e1bf87c15f372d13108fcfc9a
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-01 17:59:28 +0200

    03_Dzien_4/02_Props/05_Zadanie_5

--- 03_Dzien_4/02_Props/05_Zadanie_5/js/app.js
+++ 03_Dzien_4/02_Props/05_Zadanie_5/js/app.js
@@ -1,7 +1,37 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+class Menu extends Component {
+    render(props) {
+        return (
+            <ul>
+                {this.props.items.map(item => <li><a href={item.url}>{item.text}</a></li>)}
+            </ul>
+        )
+    }
+}
+
+const menuItems = [
+    {
+        url: "/",
+        text: "Strona główna"
+    },
+    {
+        url: "/blog",
+        text: "Blog"
+    },
+    {
+        url: "/cennik",
+        text: "Cennik"
+    },
+    {
+        url: "/kontakt",
+        text: "Kontakt"
+    }
+]
+
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <Menu items={menuItems}/>,
+    document.getElementById("app")
 );
