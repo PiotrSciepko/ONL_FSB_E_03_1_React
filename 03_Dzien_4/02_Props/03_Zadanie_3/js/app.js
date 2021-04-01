c49fb4bb74f49ecd590647f7017369790714abe8
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-01 17:41:57 +0200

    03_Dzien_4/02_Props/03_Zadanie_3

--- 03_Dzien_4/02_Props/03_Zadanie_3/js/app.js
+++ 03_Dzien_4/02_Props/03_Zadanie_3/js/app.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+const Sum = (props) => <h1>{props.numbers.reduce((a, b) => a + b)}</h1>;
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <Sum numbers={[1,2,3,4,5]}/>,
+    document.getElementById("app")
 );
