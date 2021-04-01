7312cbf148310d14b4d3dff137b7c960707c753a
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-01 17:26:44 +0200

    03_Dzien_4/02_Props/02_Zadanie_2

--- 03_Dzien_4/02_Props/02_Zadanie_2/js/app.js
+++ 03_Dzien_4/02_Props/02_Zadanie_2/js/app.js
@@ -1,7 +1,19 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+const AddNumbers = (props) => <h1>{props.numberA + props.numberB}</h1>;
+
+class AddNumbers1 extends Component {
+    render(props) {
+        return <h1>{this.props.numberA + this.props.numberB}</h1>;
+    }
+}
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <div>
+        <AddNumbers numberA={2} numberB={5}/>
+        <AddNumbers1 numberA={3} numberB={5}/>
+    </div>
+    ,
+    document.getElementById("app")
 );
