1e67a9f00129813b422a57ad088a660b343ec44a
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-01 17:03:35 +0200

    03_Dzien_4/01_Podstawy/01_Zadanie_1

--- 03_Dzien_4/01_Podstawy/01_Zadanie_1/js/app.js
+++ 03_Dzien_4/01_Podstawy/01_Zadanie_1/js/app.js
@@ -1,7 +1,32 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+const SearchBox = () => {
+    return (
+        <form>
+            <input type={"text"}/>
+            <button type={"submit"}>Wyszukaj</button>
+        </form>
+    )
+}
+
+class SearchBox1 extends Component {
+    render() {
+        return (
+            <form>
+                <input type={"text"} name={"input"}/>
+                <button type={"submit"}>Wyszukaj</button>
+            </form>
+        )
+    }
+}
+
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <div>
+        <SearchBox/>
+        <SearchBox1/>
+    </div>
+    ,
+    document.getElementById("app")
 );
