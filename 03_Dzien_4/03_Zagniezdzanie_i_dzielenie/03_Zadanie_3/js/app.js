db112d5ce3e9e8c01a98ce59903648e4d8e5280f
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-02 14:41:09 +0200

    03_Dzien_4/03_Zagniezdzanie_i_dzielenie/03_Zadanie_3

--- 03_Dzien_4/03_Zagniezdzanie_i_dzielenie/03_Zadanie_3/js/app.js
+++ 03_Dzien_4/03_Zagniezdzanie_i_dzielenie/03_Zadanie_3/js/app.js
@@ -1,7 +1,26 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+const AddNumbers = ({numberA, numberB}) => <h1>{numberA + numberB}</h1>;
+
+const App = () => <AddNumbers numberA={4} numberB={8}/>;
+
+class AddNumbers1 extends Component {
+    render() {
+        return <h1>{this.props.numberA + this.props.numberB}</h1>
+    }
+}
+
+class App1 extends Component {
+    render() {
+        return <AddNumbers1 numberA={5} numberB={8}/>;
+    }
+}
 
 
-
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(
+    <>
+        <App/>
+        <App1/>
+    </>
+    , document.getElementById("app"));
