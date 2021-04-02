12d842f1d46646e0368c90679e361ce6b75dfc3e
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-02 14:51:00 +0200

    03_Dzien_4/02_Props/01_Zadanie_1

--- 03_Dzien_4/02_Props/01_Zadanie_1/js/app.js
+++ 03_Dzien_4/02_Props/01_Zadanie_1/js/app.js
@@ -1,25 +1,22 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
-const Hello = (props) => {
-    return (
-        <h1>{props.name} {props.surname}</h1>
-    )
-}
+const Hello = ({name, surname}) => <h1>{name} {surname}</h1>
 
 class Hello1 extends Component {
-    render(props) {
+    render() {
         return (
             <h1>{this.props.name} {this.props.surname}</h1>
         )
     }
 }
 
+
 ReactDOM.render(
-    <div>
+    <>
         <Hello name="Piotr" surname="Ściepko"/>
         <Hello1 name="Piotr" surname="Ściepko"/>
-    </div>
+    </>
     ,
     document.getElementById("app")
 );
