90a3a0d7dcd4648c973ed294a893bef585ad9dea
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-02 21:17:18 +0200

    04_Dzien_5-7/01_Kompozycja_i_props_children/01_Zadanie_1

--- 04_Dzien_5-7/01_Kompozycja_i_props_children/01_Zadanie_1/js/app.js
+++ 04_Dzien_5-7/01_Kompozycja_i_props_children/01_Zadanie_1/js/app.js
@@ -1,7 +1,42 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+const ParentComponent = (props) => {
+    return (
+        <div>
+            ParentComponent
+            <ChildComponent>
+                {props.children}
+            </ChildComponent>
+        </div>
+    )
+}
+
+const ChildComponent = (props) => {
+    return (
+        <div>
+            ChildComponent
+            <GrandchildComponent>
+                {props.children}
+            </GrandchildComponent>
+        </div>
+    )
+}
+
+const GrandchildComponent = (props) => {
+    return (
+        <div>
+            GrandchildComponent
+            {props.children}
+        </div>
+    )
+}
+
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <ParentComponent>
+        <h1>To działa!</h1>
+    </ParentComponent>
+    ,
+    document.getElementById("app")
 );
