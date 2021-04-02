4a7d1138d91492b4453b1837fac69589446d0230
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-02 11:41:17 +0200

    03_Dzien_4/03_Zagniezdzanie_i_dzielenie/01_Zadanie_1

--- 03_Dzien_4/03_Zagniezdzanie_i_dzielenie/01_Zadanie_1/js/app.js
+++ 03_Dzien_4/03_Zagniezdzanie_i_dzielenie/01_Zadanie_1/js/app.js
@@ -1,7 +1,23 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+const CurrencyConverter = ({from, to, value, rate}) => {
+    return (
+        <div>
+            <strong>{value}</strong> {from} => <strong>{(Math.round(value * rate * 100) / 100).toFixed(2)}</strong> {to}
+        </div>
+    )
+}
 
+const App = () => {
+    return (
+        <>
+            <CurrencyConverter from="EUR" to="USD" value={200} rate={1.12275}/>
+            <CurrencyConverter from="USD" to="PLN" value={200} rate={3.9}/>
+            <CurrencyConverter from="EUR" to="PLN" value={200} rate={4.6}/>
+        </>
+    )
+}
 
 
 ReactDOM.render(<App/>, document.getElementById("app"));
