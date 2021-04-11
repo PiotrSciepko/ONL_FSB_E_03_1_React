5db7e52e1abd92c27b75eb101996860cdaaa216a
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-11 16:31:21 +0200

    05_Dzien_8/01_Podstawy_zdarzen/01_Zadanie_1

--- 05_Dzien_8/01_Podstawy_zdarzen/01_Zadanie_1/js/Buttons.js
+++ 05_Dzien_8/01_Podstawy_zdarzen/01_Zadanie_1/js/Buttons.js
@@ -2,17 +2,17 @@ import React, {Component} from "react";
 
 const Buttons = () => {
 
-    const getPower = ()=> {
+    const getPower = () => {
         const A = prompt("Podaj liczbę A");
         const B = prompt("Podaj liczbę B");
-        console.log(A**B)
+        console.log(A ** B)
     }
 
     return (
         <>
             <button onClick={() => console.log("Pierwszy przycisk kliknięty")}>Klik! 1</button>
             <button onClick={getPower}>Klik! 2</button>
-            <button onClick={()=> console.log(screen.width, screen.height)}>Klik! 3</button>
+            <button onClick={() => console.log(screen.width, screen.height)}>Klik! 3</button>
         </>
 
     )
