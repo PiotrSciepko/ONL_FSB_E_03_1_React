2386df37e3a68fbce59da9cecde1edaad516d9ec
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-11 16:27:35 +0200

    05_Dzien_8/01_Podstawy_zdarzen/01_Zadanie_1

---
+++ 05_Dzien_8/01_Podstawy_zdarzen/01_Zadanie_1/js/Buttons.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+const Buttons = () => {
+
+    const getPower = ()=> {
+        const A = prompt("Podaj liczbę A");
+        const B = prompt("Podaj liczbę B");
+        console.log(A**B)
+    }
+
+    return (
+        <>
+            <button onClick={() => console.log("Pierwszy przycisk kliknięty")}>Klik! 1</button>
+            <button onClick={getPower}>Klik! 2</button>
+            <button onClick={()=> console.log(screen.width, screen.height)}>Klik! 3</button>
+        </>
+
+    )
+}
+
+export default Buttons;
--- 05_Dzien_8/01_Podstawy_zdarzen/01_Zadanie_1/js/app.js
+++ 05_Dzien_8/01_Podstawy_zdarzen/01_Zadanie_1/js/app.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import Buttons from "./Buttons";
 
-
+const App = () => {
+    return <Buttons/>
+}
 
 ReactDOM.render(<App/>, document.getElementById("app"));
