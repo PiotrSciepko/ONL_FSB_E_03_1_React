0487cc64d15b8535b2ee7515e3345af8fd470870
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-14 16:50:40 +0200

    06_Dzien_9-10/01_useState/05_Zadanie_5

--- 06_Dzien_9-10/01_useState/05_Zadanie_5/js/Numbers.js
+++ 06_Dzien_9-10/01_useState/05_Zadanie_5/js/Numbers.js
@@ -4,14 +4,14 @@ const Numbers = () => {
     const nums = [54, 76, 24, 63, 4, 54, 82, 36, 13, 80, 10, 69, 4, 23, 40];
     const [numbers, setNumbers] = useState(nums);
 
-    const filterEven = () => setNumbers(nums.filter(number => number % 2 === 0))
-    const filterOdd = () => setNumbers(nums.filter(number => number % 2 !== 0))
+    const showEven = () => setNumbers(nums.filter(number => number % 2 === 0))
+    const showOdd = () => setNumbers(nums.filter(number => number % 2 !== 0))
     const showAll = () => setNumbers(nums);
 
     return (
         <>
-            <button onClick={filterEven}>Pokaż tylko parzyste</button>
-            <button onClick={filterOdd}>Pokaż tylko nieparzyste</button>
+            <button onClick={showEven}>Pokaż tylko parzyste</button>
+            <button onClick={showOdd}>Pokaż tylko nieparzyste</button>
             <button onClick={showAll}>Pokaż wszystkie</button>
             <ul>
                 {numbers.map(number => <li>{number}</li>)}
