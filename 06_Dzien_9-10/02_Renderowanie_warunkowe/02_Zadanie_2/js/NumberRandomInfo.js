bb83530664f0405dbc7ebb5bc210957c92bb96c0
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-15 13:22:12 +0200

    06_Dzien_9-10/02_Renderowanie_warunkowe/02_Zadanie_2

--- 06_Dzien_9-10/02_Renderowanie_warunkowe/02_Zadanie_2/js/NumberRandomInfo.js
+++ 06_Dzien_9-10/02_Renderowanie_warunkowe/02_Zadanie_2/js/NumberRandomInfo.js
@@ -3,18 +3,18 @@ import React from "react";
 export default class NumberRandomInfo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {num: 0}
+        this.state = {num: 0};
         setInterval(() => this.setState({num: Math.ceil(Math.random() * 5)}), 1000);
     }
 
     render() {
         let randomInfo = "3";
         if (this.state.num > 3) {
-            randomInfo = this.state.num + "  -> większa od 3"
+            randomInfo = this.state.num + "  -> większa od 3";
         } else if (this.state.num < 3) {
-            randomInfo = this.state.num + "  -> mniejsza od 3"
-        }
-        return <h1>{randomInfo}</h1>
+            randomInfo = this.state.num + "  -> mniejsza od 3";
+        };
+        return <h1>{randomInfo}</h1>;
     }
 
 }
