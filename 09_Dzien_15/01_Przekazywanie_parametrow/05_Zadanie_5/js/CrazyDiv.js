a9f93278168c12a1b9863f358926ae77e2a964e9
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-21 18:58:52 +0200

    09_Dzien_15/01_Przekazywanie_parametrow/05_Zadanie_5

---
+++ 09_Dzien_15/01_Przekazywanie_parametrow/05_Zadanie_5/js/CrazyDiv.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const CrazyDiv = () => {
+    const [style, setStyle] = useState({width: "100px", height: "100px", position: "absolute", backgroundColor: "red"});
+
+    const handleHover = (left, top) => {
+        setStyle(prev => ({...prev, left: left, top: top}))
+    }
+
+    return (
+        <div onMouseEnter={() => handleHover(Math.floor(Math.random() * (800)), Math.floor(Math.random() * (800)))}
+             style={style}>
+
+        </div>
+    );
+};
+
+export default CrazyDiv;
--- 09_Dzien_15/01_Przekazywanie_parametrow/05_Zadanie_5/js/app.js
+++ 09_Dzien_15/01_Przekazywanie_parametrow/05_Zadanie_5/js/app.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import CrazyDiv from "./CrazyDiv";
 
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<CrazyDiv/>, document.getElementById("app"));
