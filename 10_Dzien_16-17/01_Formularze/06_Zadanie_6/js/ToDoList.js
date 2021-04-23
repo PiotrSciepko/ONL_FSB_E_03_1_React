aea6a03777b9bc96461fca5c7132da7962b101f9
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-23 16:33:57 +0200

    10_Dzien_16-17/01_Formularze/06_Zadanie_6

---
+++ 10_Dzien_16-17/01_Formularze/06_Zadanie_6/js/ToDoList.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+
+const ToDoList = () => {
+    const [list, setList] = useState([{id: 1, name: "Rzecz do zrobienia", done: false}]);
+    const [newItem, setNewItem] = useState({id: '', name: '', done: ''});
+
+    const handleClick = item => {
+        setList(prev => [...prev.filter(el => el.id !== item.id), {
+            id: item.id,
+            name: item.name,
+            done: !item.done
+        }].sort((a, b) => a.id - b.id))
+    }
+
+    const handleInput = e => {
+        setNewItem({id: list.length + 1, name: e.target.value, done: false});
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (newItem.name) {
+            setList(prev => [...prev, newItem]);
+            setNewItem(prev => ({...prev, name: ''}))
+        };
+    }
+
+    return (
+        <div className={"toDoList"}>
+            <form className={"header"} onSubmit={handleSubmit}>
+                <h2>Twoja lista zadań</h2>
+                <input type="text" placeholder="np. Zrobić zakupy" value={newItem.name} onChange={handleInput}/>
+                <button className="btn-add">Dodaj</button>
+            </form>
+            <ul>
+                {list.map(item => <li key={item.id} className={item.done ? "done" : null}
+                                      onClick={() => handleClick(item)}>{item.name}</li>)}
+            </ul>
+        </div>
+    );
+};
+
+export default ToDoList;
--- 10_Dzien_16-17/01_Formularze/06_Zadanie_6/js/app.js
+++ 10_Dzien_16-17/01_Formularze/06_Zadanie_6/js/app.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import ToDoList from "./ToDoList";
 
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<ToDoList/>, document.getElementById("app"));
