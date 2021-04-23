8e4a79531d6e37c8d39f180237093a9c33330ef5
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-23 11:18:11 +0200

    10_Dzien_16-17/01_Formularze/05_Zadanie_5

---
+++ 10_Dzien_16-17/01_Formularze/05_Zadanie_5/js/SelectOrType.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react';
+
+const SelectOrType = (props) => {
+    const [items, setItems] = useState([...props.items, "inne"]);
+    const [item, setItem] = useState("BMW");
+    const [isHidden, setIsHidden] = useState(true);
+    const [newItem, setNewItem] = useState('');
+
+    useEffect(() => setIsHidden(item !== "inne"), [item]);
+    useEffect(() => setItem(newItem), [items])
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setItems(prev => [...prev.filter(item => item !== "inne"), newItem, "inne"]);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <select value={item} onChange={e => setItem(e.target.value)}>
+                    {items.map((item, index) => <option key={index} value={item}>{item}</option>)}
+                </select>
+                <input hidden={isHidden} type={"text"} value={newItem} onChange={e => setNewItem(e.target.value)}/>
+                <button hidden={isHidden} type={"submit"}>Zatwierdź</button>
+            </form>
+        </div>
+    );
+};
+
+export default SelectOrType;
--- 10_Dzien_16-17/01_Formularze/05_Zadanie_5/js/app.js
+++ 10_Dzien_16-17/01_Formularze/05_Zadanie_5/js/app.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import SelectOrType from "./SelectOrType";
 
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<SelectOrType items={["BMW", "Jaguar", "Porsche", "Volvo"]}/>, document.getElementById("app"));
