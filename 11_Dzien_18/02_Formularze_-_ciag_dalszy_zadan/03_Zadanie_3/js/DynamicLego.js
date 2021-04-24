2051a0309255a63c536e7a1a0a8bd3d9b7512665
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-24 15:22:51 +0200

    11_Dzien_18/02_Formularze_-_ciag_dalszy_zadan/03_Zadanie_3

--- 11_Dzien_18/02_Formularze_-_ciag_dalszy_zadan/03_Zadanie_3/js/DynamicLego.js
+++ 11_Dzien_18/02_Formularze_-_ciag_dalszy_zadan/03_Zadanie_3/js/DynamicLego.js
@@ -7,13 +7,13 @@ const DynamicLego = () => {
     const style = {backgroundColor: "red", height: "50px"}
 
     useEffect(() => {
-        setList([...new Array(+qty)].map((_, i) => <li key={i} style={{...style, width: `${(i+1) * 50}px`}}/>))
+        setList([...new Array(+qty)].map((_, i) => <li key={i} style={{...style, width: `${(i + 1) * 50}px`}}/>))
     }, [qty]);
 
     return (
         <div>
             <form>
-                <input type={"number"} value={qty} onChange={e => setQty(e.target.value)}/>
+                <input type={"number"} value={qty} onChange={e => e.target.value >= 0 && setQty(e.target.value)}/>
             </form>
             <ul>
                 {list}
