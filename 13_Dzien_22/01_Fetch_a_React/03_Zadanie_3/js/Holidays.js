c995498061c824ca3db161dcc47f55baa31dd5a0
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-27 21:52:48 +0200

    13_Dzien_22/01_Fetch_a_React/03_Zadanie_3

--- 13_Dzien_22/01_Fetch_a_React/03_Zadanie_3/js/Holidays.js
+++ 13_Dzien_22/01_Fetch_a_React/03_Zadanie_3/js/Holidays.js
@@ -2,19 +2,32 @@ import React, {useEffect, useState} from 'react';
 
 const Holidays = () => {
     const [holidays, setHolidays] = useState([]);
+    const [country, setCountry] = useState("PL");
 
     useEffect(() => {
-        fetch("https://fer-api.coderslab.pl/v1/holidays?key=e92601251-c0a2-4s63-v73f-54041195480f&country=GB")
-            .then(resp => resp.json())
-            .then(data => setHolidays(data.holidays))
-    }, []);
+        (async () => {
+            const resp = await fetch(`https://fer-api.coderslab.pl/v1/holidays?key=e92601251-c0a2-4s63-v73f-54041195480f&country=${country}`);
+            const data = resp.ok && await resp.json();
+            setHolidays(data.holidays);
+        })()
+    }, [country]);
 
     return (
-        <ul>
-            {holidays.map(item => <li key={item.uuid} className={'holiday-name'}><h3>{item.name}</h3>
-                <div className={'holiday-date'}>{item.date}</div>
-            </li>)}
-        </ul>
+        <div>
+            <form>
+                <select value={country} onChange={e => setCountry(e.target.value)}>
+                    <option value={"PL"}>PL</option>
+                    <option value={"GB"}>GB</option>
+                    <option value={"US"}>US</option>
+                </select>
+            </form>
+            <ul>
+                {holidays.map(item => <li key={item.uuid} className={'holiday-name'}><h3>{item.name}</h3>
+                    <div className={'holiday-date'}>{item.date}</div>
+                </li>)}
+            </ul>
+        </div>
+
     );
 };
 
