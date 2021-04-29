ac1053cd6b7324d00bdd6f45b77bab024a42df20
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-29 14:47:39 +0200

    14_Dzien_23-24/01_React_Router/04_Zadanie_4

--- 14_Dzien_23-24/01_React_Router/04_Zadanie_4/js/app.js
+++ 14_Dzien_23-24/01_React_Router/04_Zadanie_4/js/app.js
@@ -1,5 +1,31 @@
-import React, {Component} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import {HashRouter, Link, NavLink, Route, Switch} from "react-router-dom";
+import Home from "./Home";
+import Pricing from "./Pricing";
+import Blog from "./Blog";
+import NotFound from "./NotFound";
 
+const App = () => {
+    const activeStyle = {border: "3px solid green"};
+    return (
+        <div>
+            <h1>Aplikacja React z React Router</h1>
+            <HashRouter>
+                <Switch>
+                    <Route exact path={"/"} component={Home}/>
+                    <Route path={"/blog"} component={Blog}/>
+                    <Route path={"/pricing"} component={Pricing}/>
+                    <Route component={NotFound}/>
+                </Switch>
+                <ul>
+                    <li><NavLink exact to={"/"} activeStyle={activeStyle}>Home</NavLink></li>
+                    <li><NavLink to={"/blog"} activeStyle={activeStyle}>Blog</NavLink></li>
+                    <li><NavLink to={"/pricing"} activeStyle={activeStyle}>Pricing</NavLink></li>
+                </ul>
+            </HashRouter>
+        </div>
+    )
+}
 
 ReactDOM.render(<App/>, document.getElementById("app"));
