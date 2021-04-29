c1f22768528d3fb8785190f44c361d1665538114
PiotrSciepko <piotrek.priv@gmail.com>
2021-04-29 11:58:07 +0200

    14_Dzien_23-24/01_React_Router/02_Zadanie_2

---
+++ 14_Dzien_23-24/01_React_Router/02_Zadanie_2/js/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => (
+    <h1>Witaj na stronie</h1>
+);
+
+export default Home;
--- 14_Dzien_23-24/01_React_Router/02_Zadanie_2/js/app.js
+++ 14_Dzien_23-24/01_React_Router/02_Zadanie_2/js/app.js
@@ -1,5 +1,29 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import {HashRouter, Link, Route, Switch} from "react-router-dom";
+import Home from "./Home";
+import Pricing from "./Pricing";
+import Blog from "./Blog";
+import NotFound from "./NotFound";
+
+const App = () => (
+    <div>
+        <h1>Aplikacja React z React Router</h1>
+        <HashRouter>
+            <Switch>
+                <Route exact path={"/"} component={Home}/>
+                <Route path={"/blog"} component={Blog}/>
+                <Route path={"/pricing"} component={Pricing}/>
+                <Route component={NotFound}/>
+            </Switch>
+            <ul>
+                <li><Link to={"/"}>Home</Link></li>
+                <li><Link to={"/blog"}>Blog</Link></li>
+                <li><Link to={"/pricing"}>Pricing</Link></li>
+            </ul>
+        </HashRouter>
+    </div>
+)
 
 
 ReactDOM.render(<App/>, document.getElementById("app"));
